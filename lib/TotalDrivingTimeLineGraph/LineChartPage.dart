db783660654d971e816b77c68e57386f5c1a869a
Siddique Ahmad <ahmadsiddique7073@gmail.com>
2023-09-10 07:26:34 +0530

    updated_vehicleallocationScreen

---
+++ lib/TotalDrivingTimeLineGraph/LineChartPage.dart
@@ -0,0 +1,20 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:truck_tracking/widgets/LineChartWidget.dart';
+
+class LineChartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+        color: const Color(0xff020227),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: LineChartWidget(),
+        ),
+      );
+}
